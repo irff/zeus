01d68f95da1d2d0749f8407ac0e3548eadfac5b6
Gentur Waskito <genturwt@gmail.com>
2016-08-22 13:07:52 +0700

    refactoring

--- app_test.py
+++ app_test.py
@@ -3,10 +3,8 @@ import unittest
 import json
 from seeds import Seeder
 
-class AuthTest(unittest.TestCase):
-
-        
-    def setUp(self):
+class Helper():
+    def __init__(self):
         self.app = app.app.test_client()
 
     def login(self, email, password):
@@ -24,18 +22,27 @@ class AuthTest(unittest.TestCase):
             password=password
         ))
 
+    def get_model(self, model):
+        return self.app.get('/'+model)
+
+class AuthTest(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.app.test_client()
+        self.helper = Helper()
+
     def test_login(self):
         def valid_credential():
-            rv = self.login('genturwt@gmail.com', 'quint-dev')
-            assert 'success' in rv.data
+            rv = self.helper.login('genturwt@gmail.com', 'quint-dev')
+            self.assertIn('success', rv.data)
 
         def wrong_email():
-            rv = self.login('ags@hmail.com', 'quint-dev')
-            assert 'invalid' in rv.data
+            rv = self.helper.login('ags@hmail.com', 'quint-dev')
+            self.assertIn('invalid', rv.data)
 
         def wrong_password():
-            rv = self.login('genturwt@gmail.com', 'quint')
-            assert 'invalid' in rv.data
+            rv = self.helper.login('genturwt@gmail.com', 'quint')
+            self.assertIn('invalid', rv.data)
 
         valid_credential()
         wrong_email()
@@ -43,25 +50,25 @@ class AuthTest(unittest.TestCase):
 
     def test_logout(self):
         def with_login():
-            self.login('genturwt@gmail.com', 'quint-dev')
-            rv = self.logout()
-            assert 'success' in rv.data
+            self.helper.login('genturwt@gmail.com', 'quint-dev')
+            rv = self.helper.logout()
+            self.assertIn('success', rv.data)
         
         def without_login():
-            rv = self.logout()
-            assert 'success' in rv.data
+            rv = self.helper.logout()
+            self.assertIn('success', rv.data)
 
         with_login()
         without_login()
 
     def test_register(self):
         def valid_credential():
-            rv = self.register('tri@quint.dev', 'quint')
-            assert 'created' in rv.data
+            rv = self.helper.register('tri@quint.dev', 'quint')
+            self.assertIn('created', rv.data)
         
         def duplicate_email():
-            rv = self.register('genturwt@gmail.com', 'quint')
-            assert 'exist' in rv.data
+            rv = self.helper.register('genturwt@gmail.com', 'quint')
+            self.assertIn('exist', rv.data)
 
         valid_credential()
         duplicate_email()
@@ -69,23 +76,23 @@ class AuthTest(unittest.TestCase):
 class ModelTest(unittest.TestCase):
     def setUp(self):
         self.app = app.app.test_client()
-
-    def get_model(self, model):
-        rv = self.app.get('/'+model)
-        data = json.loads(rv.data)
-        assert len(data) != 0
+        self.helper = Helper()
 
     def test_student(self):
-        self.get_model('students')
+        rv = self.helper.get_model('students')
+        self.assertIsNotNone(rv)
 
     def test_user(self):
-        self.get_model('users')
+        rv = self.helper.get_model('users')
+        self.assertIsNotNone(rv)
 
     def test_company(self):
-        self.get_model('companies')
+        rv = self.helper.get_model('companies')
+        self.assertIsNotNone(rv)
 
     def test_application(self):
-        self.get_model('applications')
+        rv = self.helper.get_model('applications')
+        self.assertIsNotNone(rv)
 
 
 if __name__ == '__main__':
