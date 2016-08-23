dc78051ddb8293f1bd0de04184f7ac4e01f68556
Gentur Waskito Triwinasis <genturwt@gmail.com>
2016-08-23 00:59:32 -0500

    Basic Authentication and Register Feature (#5)
    add basic login and logout feature
    add basic register feature
    endpoint API:
    
    POST /login [email, password]
    GET /logout
    POST /register [email, password]

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify, request, render_template, Response
+from flask import Flask, jsonify, request, render_template, Response, redirect, url_for
 from flask_mongoengine import MongoEngine
 from flask_dotenv import DotEnv
+from flask_login import LoginManager, login_user, login_required, logout_user
+from werkzeug.security import check_password_hash, generate_password_hash
 
 import json
 
@@ -8,13 +10,63 @@ from models import *
 from seeds import Seeder
 
 env = DotEnv()
+login_manager = LoginManager()
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.secret_key = 'QuintDev'
 env.init_app(app)
+login_manager.init_app(app)
 db = MongoEngine(app)
 
-seeder = Seeder()
-seeder.seed()
+# LOGIN, LOGOUT, REGISTER
+@login_manager.user_loader
+def load_user(user_id):
+    return User.objects(id=user_id).first()
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return jsonify({
+        'status': 'Unauthorized Area'
+    }), 401
+
+@app.route("/login", methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+    user = User.objects(email=email).first()
+
+    if user == None or not check_password_hash(user.password, password):
+        return jsonify({
+            'status': 'invalid credentials'
+        }), 403
+    else:
+        login_user(user)
+        return jsonify({
+            'status': 'logged in successfully'
+        }), 200
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return jsonify({
+        'status': 'logged out successfully'
+    }), 200
+
+@app.route("/register", methods=['POST'])
+def register():
+    email = request.form['email']
+    password = request.form['password']
+
+    try:
+        user = User(email=email, password=generate_password_hash(password))
+        user.save()
+        return jsonify({
+            'status': 'account created'
+        }), 201
+    except NotUniqueError:
+        return jsonify({
+            'status': 'email already exist'
+        }), 403
 
 # ROUTE TESTING
 @app.route("/")
@@ -24,23 +76,22 @@ def hello():
 @app.route("/students")
 def students():
     students = Student.objects().first()
-    return Response(json.dumps(students.serialize()), mimetype='application/json')
+    return jsonify(students.serialize()), 200
 
 @app.route("/users")
 def users():
     users = User.objects().first()
-    return Response(json.dumps(users.serialize()), mimetype='application/json')
+    return jsonify(users.serialize()), 200
 
 @app.route("/companies")
 def companies():
     companies = Company.objects().first()
-    return Response(json.dumps(companies.serialize()), mimetype='application/json')
+    return jsonify(companies.serialize()), 200
 
 @app.route("/applications")
 def applications():
     applications = Application.objects().first()
-    return Response(json.dumps(applications.serialize()), mimetype='application/json')
-
+    return jsonify(applications.serialize()), 200
 
 if __name__ == "__main__":
     app.run()
