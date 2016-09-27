62dbb82ce8abeea3cdaa9bb4418a7e8c23a89b9d
Gentur Waskito <genturwt@gmail.com>
2016-09-27 22:54:07 +0700

    remove unnecessary validation

--- forms.py
+++ forms.py
@@ -1,4 +1,7 @@
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
+import wtforms_json
+
+wtforms_json.init()
 
 class RegistrationForm(Form):
     email = StringField('Email', [
@@ -6,16 +9,13 @@ class RegistrationForm(Form):
         validators.DataRequired()
     ])
     password = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm_password', 'Field must be equal to Confirm Password')
+        validators.DataRequired()
     ])
-    confirm_password = PasswordField('Confirm Password')
 
     def serialize_error(self):
         return {
             'email': self.email.errors,
             'password': self.password.errors,
-            'confirm_password': self.confirm_password.errors
         }
 
 class LoginForm(Form):
