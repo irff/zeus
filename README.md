1c3226123440cd6313e2a403dd8c4328c929ebee
Unknown <genturwt@gmail.com>
2016-11-18 20:28:39 +0700

    rename /views to /html

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# zeus
-Quint backend
+# Zeus - Backend Part of Quint
+
+## Installation
+
+1. Ensure you have virtualenv for Python 2.7
+2. Clone the repo
+3. Ensure you have MongoDB installed.
+4. Ensure you have Redis installed
+5. Go into virtualenv environment
+6. Run `pip install -r requirements.txt` in the root directory
+7. Test that everything is working properly using `python app_test.py`
+8. If everything is working, you can start the server using the following steps:
+    - `export FLASK_APP=app.py`
+    - `flask run`
--- zeus/__init__.py
+++ zeus/__init__.py
@@ -5,31 +5,28 @@ from celery import Celery
 from flask_mail import Mail
 from flask_cors import CORS
 
-app = Flask('Quint API', template_folder='zeus/views')
+app = Flask('Quint API', template_folder='zeus/html')
 env = DotEnv()
 env.init_app(app)
 app.secret_key = 'QuintDev'
 CORS(app, origins=['https://*.quint.id', 'https://quint.id'])
-# Celery configuration
+
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
-# Flask-Mail configuration
 app.config['MAIL_SERVER'] = 'smtp.zoho.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'notification@quint.id'
 app.config['MAIL_PASSWORD'] = '@x8Wh@MZ'
 app.config['MAIL_DEFAULT_SENDER'] = ('Notification from Quint', 'notification@quint.id')
-app.config['TESTING'] = False
-# initiate celery
+app.config['TESTING'] = True
+
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
-# initiate connection to database
 connect('quint')
 
-# initiate mail
 mail = Mail(app)
 
 import api.student
