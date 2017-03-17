f200dee17f87884f117191113f75d92ef647cb81
Salsabila Nadhifah <salsanads@gmail.com>
2017-03-18 00:05:37 +0700

    hotfix arguments

--- zeus/api/company.py
+++ zeus/api/company.py
@@ -128,7 +128,9 @@ def modify_company(company_id):
 def get_jobs_applications(company_id):
     try:
         token_data = auth.extract_data(request.headers)
-        is_new_application = bool(request.args.get('new')) == True
+        is_new_application = False
+        if 'new' in request.args:
+            is_new_application = request.args.get('new').lower() == 'true'
         applications = _company.get_jobs_applications(company_id=company_id, is_new_application=is_new_application)
 
         token = auth.create_token({
